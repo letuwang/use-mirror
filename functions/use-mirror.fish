ea66d446a5061d5c24ea67e853dd90f38a2cd3b8
Letu Wang <wangletu57@gmail.com>
2024-06-19 11:12:26 +0800

    remove unnecessary lines

--- functions/use-mirror.fish
+++ functions/use-mirror.fish
@@ -5,11 +5,8 @@ function use-mirror
         # Set Homebrew to use Chinese mirrors
         set -Ux HOMEBREW_API_DOMAIN "https://mirrors.aliyun.com/homebrew-bottles/api"
         set -Ux HOMEBREW_BREW_GIT_REMOTE "https://mirrors.aliyun.com/homebrew/brew.git"
-        set -Ux HOMEBREW_CORE_GIT_REMOTE "https://mirrors.aliyun.com/homebrew/homebrew-core.git"
         set -Ux HOMEBREW_BOTTLE_DOMAIN "https://mirrors.aliyun.com/homebrew/homebrew-bottles"
 
-        brew tap -v --custom-remote --force-auto-update homebrew/core https://mirrors.aliyun.com/homebrew/homebrew-core.git
-        brew tap -v --custom-remote --force-auto-update homebrew/cask https://mirrors.aliyun.com/homebrew/homebrew-cask.git
         brew tap -v --custom-remote --force-auto-update homebrew/cask-fonts https://mirrors.aliyun.com/homebrew/homebrew-cask-fonts.git
         brew tap -v --custom-remote --force-auto-update homebrew/cask-versions https://mirrors.aliyun.com/homebrew/homebrew-cask-versions.git
         brew tap -v --custom-remote --force-auto-update homebrew/command-not-found https://mirrors.aliyun.com/homebrew/homebrew-command-not-found.git
@@ -29,7 +26,7 @@ function use-mirror
 
         # Reset all taps
         set -l BREW_TAPS (brew tap | tr '\n' ' ')
-        for tap in core cask cask-fonts cask-versions command-not-found services
+        for tap in cask cask-fonts cask-versions command-not-found services
             if contains "homebrew/$tap" $BREW_TAPS
                 brew tap --custom-remote "homebrew/$tap" "https://github.com/Homebrew/homebrew-$tap"
             end
