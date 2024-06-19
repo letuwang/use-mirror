6184a2e6a813655a608db218d414f2690438f03f
Letu Wang <wangletu57@gmail.com>
2024-06-19 11:42:11 +0800

    remove unnecessary taps

--- functions/use-mirror.fish
+++ functions/use-mirror.fish
@@ -7,8 +7,6 @@ function use-mirror
         set -Ux HOMEBREW_BREW_GIT_REMOTE "https://mirrors.aliyun.com/homebrew/brew.git"
         set -Ux HOMEBREW_BOTTLE_DOMAIN "https://mirrors.aliyun.com/homebrew/homebrew-bottles"
 
-        brew tap -v --custom-remote --force-auto-update homebrew/cask-fonts https://mirrors.aliyun.com/homebrew/homebrew-cask-fonts.git
-        brew tap -v --custom-remote --force-auto-update homebrew/cask-versions https://mirrors.aliyun.com/homebrew/homebrew-cask-versions.git
         brew tap -v --custom-remote --force-auto-update homebrew/command-not-found https://mirrors.aliyun.com/homebrew/homebrew-command-not-found.git
         brew tap -v --custom-remote --force-auto-update homebrew/services https://mirrors.aliyun.com/homebrew/homebrew-services.git
 
@@ -26,7 +24,7 @@ function use-mirror
 
         # Reset all taps
         set -l BREW_TAPS (brew tap | tr '\n' ' ')
-        for tap in cask cask-fonts cask-versions command-not-found services
+        for tap in command-not-found services
             if contains "homebrew/$tap" $BREW_TAPS
                 brew tap --custom-remote "homebrew/$tap" "https://github.com/Homebrew/homebrew-$tap"
             end
