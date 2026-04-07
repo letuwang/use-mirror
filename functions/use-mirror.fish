448a040980e864dd53802e624ed13b06b1c29edd
Letu Wang <git@letuwang.me>
2026-04-07 17:21:10 +0800

    Fix persistent mirror toggles

--- functions/use-mirror.fish
+++ functions/use-mirror.fish
@@ -5,13 +5,13 @@ function use-mirror
         # Homebrew
         set -Ux HOMEBREW_API_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
         set -Ux HOMEBREW_BREW_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+        set -Ux HOMEBREW_CORE_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
         set -Ux HOMEBREW_BOTTLE_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
         set -Ux HOMEBREW_PIP_INDEX_URL "https://pypi.tuna.tsinghua.edu.cn/simple"
-
-        brew tap --custom-remote homebrew/command-not-found https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-command-not-found.git
-        brew tap --custom-remote homebrew/services https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-services.git
-
-        brew update
+        if command -q brew
+            brew untap homebrew/command-not-found >/dev/null 2>/dev/null
+            brew untap homebrew/services >/dev/null 2>/dev/null
+        end
 
         # pip
         set -Ux PIP_INDEX_URL "https://pypi.tuna.tsinghua.edu.cn/simple"
@@ -33,15 +33,13 @@ function use-mirror
         # Homebrew
         set -e HOMEBREW_API_DOMAIN
         set -e HOMEBREW_BREW_GIT_REMOTE
+        set -e HOMEBREW_CORE_GIT_REMOTE
         set -e HOMEBREW_BOTTLE_DOMAIN
         set -e HOMEBREW_PIP_INDEX_URL
-
-        git -C (brew --repo) remote set-url origin https://github.com/Homebrew/brew
-
-        brew tap --custom-remote homebrew/command-not-found https://github.com/Homebrew/homebrew-command-not-found
-        brew tap --custom-remote homebrew/services https://github.com/Homebrew/homebrew-services
-
-        brew update
+        if command -q brew
+            brew untap homebrew/command-not-found >/dev/null 2>/dev/null
+            brew untap homebrew/services >/dev/null 2>/dev/null
+        end
 
         # pip
         set -Ux PIP_INDEX_URL "https://pypi.org/simple"
@@ -57,6 +55,7 @@ function use-mirror
 
         # rust
         set -e RUSTUP_UPDATE_ROOT
+        set -e RUSTUP_DIST_SERVER
 
     else
         echo "Usage: use-mirror china|us"
